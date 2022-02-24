081b5ce06ef6773ae2440bd151997116e24b1fc3
SuNG0D <46067678+SuNG0D@users.noreply.github.com>
2022-02-24 08:47:07 +0000

    Copiar archivos CSS y JS

--- script.js
+++ script.js
@@ -27,3 +27,70 @@ const typeColors = {
 };
 
 
+const searchPokemon = event => {
+  event.preventDefault();
+const { value } = event.target.pokemon;
+fetch('https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}')
+.then(data => data.json())
+.then(response => renderPokemonData(response))
+.catch(err => renderNotFound())
+}
+
+const renderPokemonData = data => {
+  const sprite = data.spirtes.front_default;
+  const { stats, types } = data;
+
+  pokeName.textContent = data.name;
+  pokeImg.setAttribute('src', sprite);
+  pokeId.textContent = 'Nº ${data.id}';
+  setCardColor(types);
+  renderPokemonTypes(types);
+  renderPokemonStats(stats);
+}
+
+
+const setCardColor = types => {
+const colorOne = typeColors[types[0].type.name];
+const colorTwo = types [1]? typeColors[types[1].type.name] : typeColors.default;
+pokeImg.style.background = 'radial-gradient(${colorTwo}33%, ${colorOne}33%)';
+pokeImg.style.backgroundsize = '5px 5px';
+}
+
+const renderPokemonTypes = types => {
+  poke.innerHTML ='';
+  types.forEach(type => {
+    const typeTextElemente = document.createElement("div");
+    typeTextElement.style.color = typeColors[type.type.name];
+    typeTextElement.textContent = type.type.name;
+    pokeTypes.appendChild(typeTextElement);
+  });
+}
+
+
+
+const renderPokemonStats = stats => {
+  pokeStats.innerHTML = '';
+  stats.forEach(stat => {
+    const statElement = document.createElement("div");
+    const statElementName = document.createElement("div");
+    const statElementAmount = document.createElement("div");
+    statElementName.textContent = stat.stat.name;
+    statElementAmount.textContent = stat.base_stat;
+    statElement.appendChild(statElementName);
+    statElement.appendChild(statElementAmount);
+    pokeStats.appendChild(statElement);
+  });
+}
+
+
+
+
+const renderNotFound = () => {
+  pokeName.textContent = 'No encontrado';
+  pokeImg.setAttribute('src', 'poke-shadow.png');
+  pokeImg.style.background = '#fff';
+  pokeTypes.innerHTML = '';
+  pokeStats.innerHTML = '';
+  pokeId.textContent = '';
+}
+
